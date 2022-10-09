ffd7abc6d246fab4e4f46b96f713887d0f4c7156
James Dycus <dycus.j.jd@gmail.com>
2022-10-08 21:16:00 -0400

    created login template, created the login class view,  connected to a url path

--- robotics_app/login_app/views.py
+++ robotics_app/login_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.
+from django.views.generic import TemplateView, FormView
 
 # Create your views here.
 
 class LoginView(TemplateView):
+    template_name = 'login_app/login.html'
