0505a79e96ee39cb5c043e78f8b252aa34fb2197
James Dycus <dycus.j.jd@gmail.com>
2022-10-08 21:34:42 -0400

    added gitignore and requirements

--- robotics_app/robotics_app/settings.py
+++ robotics_app/robotics_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-93(dj2r*^zs*em0$1uw@w0-h_kz*0pij7dwvw*(psu^(=!(k-!'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
