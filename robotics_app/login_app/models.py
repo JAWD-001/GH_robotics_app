e0e6897ed2fc441baba0dc2834ff18804bd39d21
James Dycus <dycus.j.jd@gmail.com>
2022-10-09 12:44:53 -0400

    added user model and model fields with some field options

--- robotics_app/login_app/models.py
+++ robotics_app/login_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Users(modesls.Model):
+    id = models.BigAutoField(primary_key=True)
+    first_name = models.Charfield(max_length=50, verbose_name='First Name:', blank=False)
+    last_name = models.Charfield(max_length=50, verbose_name='Last Name:', blank=False)
+    username = models.Charfield(max_length=50, unique=True, blank=False)
+    passwrod = models.Charfield(max_length=50, blank=False)
+    email = models.EmailField(unique=True, blank=False)
+    
