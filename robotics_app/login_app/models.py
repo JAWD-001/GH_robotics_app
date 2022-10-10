1648bd85da322dd941170a5c5c85389dc6642ece
James Dycus <dycus.j.jd@gmail.com>
2022-10-09 20:51:11 -0400

    fixed password model field bug

--- robotics_app/login_app/models.py
+++ robotics_app/login_app/models.py
@@ -6,6 +6,6 @@ class Users(models.Model):
     first_name = models.CharField(max_length=50, verbose_name='First Name:', blank=False)
     last_name = models.CharField(max_length=50, verbose_name='Last Name:', blank=False)
     username = models.CharField(max_length=50, unique=True, blank=False)
-    passwrod = models.CharField(max_length=50, blank=False)
+    password = models.CharField(max_length=50, blank=False)
     email = models.EmailField(unique=True, blank=False)
     
