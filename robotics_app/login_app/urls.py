eb555b6005fafe219acc67d914f2a3e6fa6d99f0
James Dycus <dycus.j.jd@gmail.com>
2022-10-11 13:43:57 -0400

    added user_form template, UserCreateView view, and made the create user path. Also included links to creating an account on the login template

--- robotics_app/login_app/urls.py
+++ robotics_app/login_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import LoginFormView
+from .views import LoginFormView, UserCreateView
 
 app_name = 'login'
 
 urlpatterns =[
     path('', LoginFormView.as_view(), name='login'),
+    path('create', UserCreateView.as_view(), name='create')
 ]
--- robotics_app/login_app/views.py
+++ robotics_app/login_app/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, FormView
+from django.views.generic import TemplateView, FormView, CreateView
 from login_app.forms import LoginForm
+from login_app.models import Users
+from django.urls import reverse_lazy
 
 # Create your views here.
 
 class LoginFormView(FormView):
     form_class = LoginForm
     template_name = 'login_app/login.html'
+    
+class UserCreateView(CreateView):
+    model = Users
+    template_name = 'login_app/user_form.html'
+    fields = '__all__'
+    success_url = reverse_lazy('login_app:login.html')
+    
