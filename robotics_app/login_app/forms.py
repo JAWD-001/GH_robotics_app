4a4d7cca18e3531c422090f837b7f8eb00e81ae7
James Dycus <dycus.j.jd@gmail.com>
2022-10-21 12:23:48 -0400

    eliminated field tags and changed styling for the input forms. Also changed some HTML layout.

--- robotics_app/login_app/forms.py
+++ robotics_app/login_app/forms.py
@@ -1,8 +1,13 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput
+from django import forms 
 from .models import Users
 
 class LoginForm(ModelForm):
     class Meta:
         model = Users
         fields = ['username', 'password']
+        widgets = {
+            'username':TextInput(attrs={'placeholder': 'Username'}),
+            'password': TextInput(attrs={'placeholder': 'Password'}),
+        }
     
