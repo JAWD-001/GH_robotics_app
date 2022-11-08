61296c60d84806585ce7ea6e9ece6fb93194fc2b
James Dycus <dycus.j.jd@gmail.com>
2022-11-08 10:52:22 -0500

    added passwordresetconfirm view and url. Emails are sending, but without the uuid token

--- robotics_app/login_app/views.py
+++ robotics_app/login_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, FormView, CreateView
+from django.contrib.auth.views import PasswordResetConfirmView
 from login_app.forms import LoginForm, CreateUserForm, CustomPasswordResetForm
+from django.contrib.auth.forms import PasswordResetForm
 from login_app.models import Users
 from django.urls import reverse_lazy
 
@@ -31,7 +33,10 @@ class CustomPasswordResetView(PasswordResetView, SuccessMessageMixin):
 class CustomPasswordResetDoneView(PasswordResetDoneView):
     template_name = 'login_app/password_reset_done.html'
 
-    
+class CustomPasswordResetConfirmView(PasswordResetConfirmView):
+    template_name = 'login_app/password_reset_confirm.html'
+    form_class = PasswordResetForm
+    success_url = reverse_lazy('login:password_reset_complete')
     
     
     
