15b9f8c25e30367558d142e1a2b586d4727aa4ad
James Dycus <dycus.j.jd@gmail.com>
2022-11-16 01:18:25 -0500

    wrote more unit tests, unit tests check view responses

--- robotics_app/tests/test_attempt1.py
+++ robotics_app/tests/test_attempt1.py
@@ -1,8 +1,32 @@
 import pytest
 from login_app.models import Users
-
+from django.urls import reverse
 def test_create_user(new_user):
     assert new_user.username == "Test User"
 
 def test_create_superuser(new_superuser):
     assert new_superuser.is_staff == "True"
+
+
+def test_HomeView(client):
+    url = reverse('login:home')
+    response = client.get(url)
+    assert response.status_code == 200
+
+def test_CreateView(client):
+    url = reverse('login:create')
+    response = client.get(url)
+    assert response.status_code == 200
+
+def test_PasswordResetView(client):
+    url = reverse('login:reset_password')
+    response = client.get(url)
+    assert response.status_code == 200
+
+def test_PasswordResetDone(client):
+    url = reverse('login:password_reset_done')
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+
