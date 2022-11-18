436ce71d265b19c1f1ca488087930a222c57db80
James Dycus <dycus.j.jd@gmail.com>
2022-11-18 12:53:56 -0500

    made a unit test to view the password_reset_complete

--- robotics_app/tests/test_login_app.py
+++ robotics_app/tests/test_login_app.py
@@ -1,6 +1,9 @@
 import pytest
 from login_app.models import Users
 from django.urls import reverse
+
+
+#Tests for User and Superuser Creation
 def test_create_user(new_user):
     assert new_user.username == "Test User"
 
@@ -8,6 +11,8 @@ def test_create_superuser(new_superuser):
     assert new_superuser.is_staff == "True"
 
 
+
+#View Unit Tests
 def test_HomeView(client):
     url = reverse('login:home')
     response = client.get(url)
@@ -28,6 +33,11 @@ def test_PasswordResetDoneView(client):
     response = client.get(url)
     assert response.status_code == 200
 
+def test_PasswordResetCompleteView(client):
+    url = reverse('login:password_reset_complete')
+    response = client.get(url)
+    assert response.status_code == 200
+
 def test_UsernameFormView(client):
     url = reverse('login:forgot_username')
     response = client.get(url)
@@ -37,4 +47,3 @@ def test_ForgotUsernameDoneView(client):
     url = reverse('login:forgot_username_done')
     response = client.get(url)
     assert response.status_code == 200
-
