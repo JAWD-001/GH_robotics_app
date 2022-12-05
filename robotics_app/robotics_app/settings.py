4d13b74c28f2d2f102fbae1ad4f09216eda81b03
James Dycus <dycus.j.jd@gmail.com>
2022-12-05 00:40:05 -0500

    changed envirnonment variable settings

--- robotics_app/robotics_app/settings.py
+++ robotics_app/robotics_app/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
 import os
 import environ
 
@@ -26,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
