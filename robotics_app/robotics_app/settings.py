5e7e5345a716188cc27d18a1b0da5a7faf8f1660
James Dycus <dycus.j.jd@gmail.com>
2022-12-05 02:04:18 -0500

    dropped environ variable

--- robotics_app/robotics_app/settings.py
+++ robotics_app/robotics_app/settings.py
@@ -13,10 +13,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 from decouple import config
 import os
-import environ
-
-env = environ.Env()
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
