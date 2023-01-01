2847565827c084ef116aa775c27c1145c66ff2d1
James Dycus <dycus.j.jd@gmail.com>
2022-12-31 22:58:38 -0500

    testing

--- robotics_app/build_summaries/models.py
+++ robotics_app/build_summaries/models.py
@@ -1,22 +1,5 @@
 from django.db import models
 from datetime import date
 
-class BuildSummary(models.Model):
-        class SubTeamNames(models.TextChoices):
-                CODING = 'CD'
-                ENGINEERING = 'EG'
-                DRIVETRAIN = 'DT'
-                SPIRIT = 'SP'
-
-        id = models.BigAutoField(primary_key=True)
-        subteam = models.CharField(max_length=2, choices=SubTeamNames.choices, default=SubTeamNames.ENGINEERING)
-        summary_date = models.DateField(default=date.today)
-        daily_goal = models.TextField(blank=True)
-        work_accomplished = models.TextField(blank=True)
-        challenges_faced = models.TextField(blank=True)
-        tomorrow_goal = models.CharField(max_length=500, blank=True)
-        build_summary_image = models.ImageField(blank=True, upload_to="build_images")
-
-
 
 # Create your models here.
