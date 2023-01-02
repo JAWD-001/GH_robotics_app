f6eec4547d689951f61be0167ed83937fb9ca946
James Dycus <dycus.j.jd@gmail.com>
2023-01-02 00:45:35 -0500

    added model fields to scouting reports. Added three textinput choice classes and completed model fields for application. waiting for review before migrating

--- robotics_app/scouting_reports/models.py
+++ robotics_app/scouting_reports/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from datetime import date
 # Create your models here.
 class OpposingTeam(models.Model):
     id = models.BigAutoField(primary_key=True)
@@ -10,5 +10,29 @@ class OpposingTeam(models.Model):
 
 
 class ScoutingReport(models.Model):
+    class Option(models.TextChoices):
+        YES = 'Y'
+        NO = 'N'
+
+    class ConePlacementLocations(models.TextChoices):
+        STATION = 'Station'
+        JUNCTIONS = 'Junction'
+        POLE = 'Pole'
+        HIGH_POLE = 'High Pole'
+
+    class StackLevels(models.TextChoices):
+        FLOOR = 'Floor'
+        LOW_LEVEL = 'Low'
+        MEDIUM_LEVEL = 'Medium'
+        HIGH_LEVEL = 'High'
+
     id = models.BigAutoField(primary_key=True)
-    team = models.CharField()
+    date = models.DateField(default=date.today)
+    team = models.ForeignKey(OpposingTeam.team_name, on_delete=models.CASCADE)
+    autonomous_scoring = models.Charfield(blank=True, max_length=2, choices=Option.choices, default=Option.NO)
+    autonomous_cone_placement = models.Charfield(blank=True, max_length=12, choices=ConePlacementLocations.choices)
+    parked_autonomously = models.CharField(blank=True, max_length=2, choices=Option.choices, default=Option.NO)
+    clear_out_waittime = models.CharField(blank=True, max_length=2, choices=Option.choices, default=Option.NO)
+    side_cone_stacks = models.CharField(blank=True, max_length=2, choices=Option.choices, default=Option.NO)
+    cone_stacks_level = models.CharField(blank=True, max_length=2, choices=StackLevels.choices)
+    circuit_completed = models.CharField(blank=True, max_length=2, choices=Option.choices, default=Option.NO)
