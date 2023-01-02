77ac6497b5bfbf1752fdac00f850e638ff27b565
James Dycus <dycus.j.jd@gmail.com>
2023-01-02 13:36:11 -0500

    added forms.py and imports for each application

---
+++ robotics_app/build_summaries/forms.py
@@ -0,0 +1,4 @@
+from django.forms import ModelForm, TextInput
+
+from django import forms
+from .models import BuildSummary
---
+++ robotics_app/scouting_reports/forms.py
@@ -0,0 +1,5 @@
+from django.forms import ModelForm, TextInput
+
+from django import forms
+from .models import ScoutingReport, OpposingTeam
+
