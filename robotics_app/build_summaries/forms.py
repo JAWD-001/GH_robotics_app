32a68fca3bf2d817b91105e42c3b32b396aded47
James Dycus <dycus.j.jd@gmail.com>
2023-01-13 09:54:08 -0500

    fixed pagination bug, now has all features

--- robotics_app/build_summaries/forms.py
+++ robotics_app/build_summaries/forms.py
@@ -2,3 +2,11 @@ from django.forms import ModelForm, TextInput
 
 from django import forms
 from .models import BuildSummary
+
+class BuildSummaryForm(forms.ModelForm):
+    class Meta:
+        model = BuildSummary
+        fields = ['subteam', 'summary_date', 'daily_goal', 'work_accomplished', 'challenges_faced', 'tomorrow_goal', 'build_summary_image']
+        widgets = {
+            'text': forms.
+        }
