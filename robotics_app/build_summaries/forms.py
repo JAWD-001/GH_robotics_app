a0559d0079caa1ed0d28bfd5d469718b0b6322ca
James Dycus <dycus.j.jd@gmail.com>
2023-01-18 09:18:35 -0500

    added label for image upload input field on build summaries

--- robotics_app/build_summaries/forms.py
+++ robotics_app/build_summaries/forms.py
@@ -3,10 +3,12 @@ from django.forms import ModelForm, TextInput
 from django import forms
 from .models import BuildSummary
 
-class BuildSummaryForm(forms.ModelForm):
+'''class BuildSummaryForm(forms.ModelForm):
+    build_summary_image = ImageField(widgets = forms.ImageField(attrs={class}))
     class Meta:
         model = BuildSummary
         fields = ['subteam', 'summary_date', 'daily_goal', 'work_accomplished', 'challenges_faced', 'tomorrow_goal', 'build_summary_image']
-        widgets = {
+        widgets = forms.ImageField(attrs={})
             'text': forms.
         }
+'''
